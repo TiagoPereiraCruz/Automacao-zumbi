4bc32a62c1c3f14b9afa6a27f2a0d31e5c69806f
TiagoPereiraCruz <tiago.peng@gmail.com>
2019-12-11 09:43:19 -0300

    Screenshots com anexo para todos os testes

--- hooks/globals.js
+++ hooks/globals.js
@@ -4,8 +4,24 @@ module.exports = {
     done()
   },
 
-  afterEach: (browser, done) => {
-    browser.end()
+  afterEach: function (browser, done) {
+
+    const faker = require('faker')
+
+    let shotPath = `./tests_output/screenshots/${faker.random.uuid()}.png`
+
+    browser
+      .saveScreenshot(shotPath, function () {
+        const assertions = browser.currentTest.results.assertions || [];
+        if (assertions.length > 0) {
+          const currentAssertion = assertions[assertions.length - 1];
+          if (currentAssertion) {
+            currentAssertion.screenshots = currentAssertion.screenshots || [];
+            currentAssertion.screenshots.push(shotPath);
+          }
+        }
+      })
+      .end()
     done()
   },
 }
